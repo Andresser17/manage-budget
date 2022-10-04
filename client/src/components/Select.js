a2bd89e999269f193f49e55862b0be5fe8a54298
Andresser17 <andresserserrano2020@gmail.com>
2022-10-04 09:53:11 -0400

    feat: add Select component

--- client/src/components/Select.js
+++ client/src/components/Select.js
@@ -1,35 +1,153 @@
-import { useEffect } from "react";
-// Helpers
-import key from "../helpers/key";
+import { useState, useEffect, useRef } from "react";
+import { useController } from "react-hook-form";
+import PropTypes from "prop-types";
 // Styles
 import styles from "./Select.module.css";
 
-function Select({ options, id, selected, setSelected }) {
-  const mapped = options.map((o) => (
-    <option key={key(o.text)} value={o.value}>
-      {o.text}
-    </option>
-  ));
-
-  // set default value
-  useEffect(() => {
-    if (!selected[id] || selected[id].length === 0) {
-      options[0].value
-        ? setSelected((prev) => ({ ...prev, [id]: options[0].value }))
-        : setSelected((prev) => ({ ...prev, [id]: "default" }));
-    }
-  }, [id, selected, options, setSelected]);
-
+function Option({ option, selected, setSelected }) {
   return (
-    <select
-      onChange={(e) =>
-        setSelected((prev) => ({ ...prev, [id]: e.target.value }))
-      }
-      className={`${styles["select"]} dark`}
-    >
-      {mapped}
-    </select>
+    <span className={styles["option"]} onClick={() => setSelected(option)}>
+      {option.label}
+    </span>
   );
 }
+
+function Dropdown({
+  search = false,
+  inputValue = "",
+  options,
+  selected,
+  setSelected,
+}) {
+  return (
+    <div className={styles["dropdown"]}>
+      {options
+        .filter((op) => {
+          const filter = inputValue.toUpperCase();
+          if (search) {
+            if (op.label.toUpperCase().indexOf(filter) > -1) {
+              return op;
+            }
+
+            return undefined;
+          }
+
+          return op;
+        })
+        .map((op) => (
+          <Option
+            key={op.value}
+            option={op}
+            selected={selected}
+            setSelected={setSelected}
+          />
+        ))}
+    </div>
+  );
+}
+
+function Select({
+  options = [],
+  label = "",
+  placeholder = "Search",
+  getValue = (value) => undefined,
+  setValue = (name, value) => undefined,
+  ...props
+}) {
+  const {
+    field,
+    fieldState: { error },
+  } = useController(props);
+  const [isSearching, setIsSearching] = useState(false);
+  const [selected, setSelected] = useState({ label: "", value: "" });
+  const [isFocus, setIsFocus] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+  // Refs
+  const containerRef = useRef();
+  const outsideSpanRef = useRef();
+  const inputRef = useRef();
+
+  const handleSelection = (option) => {
+    if (isSearching) setIsSearching(false);
+    setShowDropdown(false);
+    setSelected(option);
+    setIsFocus(false);
+    getValue((prev) => ({ ...prev, [props.name]: option.value }));
+    setValue(props.name, option.label);
+  };
+
+  return (
+    <>
+      <div className={styles["container"]}>
+        <label htmlFor={props.name} className={styles["title"]}>
+          {label}
+        </label>
+        <div
+          ref={containerRef}
+          onClick={() => {
+            if (!isFocus) setIsFocus(true);
+            inputRef.current.focus();
+            setShowDropdown((prev) => !prev);
+          }}
+          className={`${styles["select"]} ${
+            isFocus ? styles["select-focus"] : ""
+          }`}
+        >
+          <input
+            autoComplete="off"
+            type="search"
+            className={styles["input"]}
+            {...{
+              placeholder,
+              ...{
+                ...field,
+                ref(e) {
+                  field.ref(e);
+                  inputRef.current = e;
+                },
+              },
+              onChange: (e) => {
+                if (!isSearching) setIsSearching(true);
+                field.onChange(e);
+              },
+              value: field.value || "",
+            }}
+          />
+        </div>
+        {showDropdown && (
+          <Dropdown
+            search={isSearching}
+            inputValue={field.value}
+            options={options}
+            {...{ selected, setSelected: handleSelection }}
+          />
+        )}
+        {/* Description */}
+        <span className={styles["description"]}>
+          {error?.type === "required" ? "This field is required" : ""}
+          {error?.message}
+        </span>
+      </div>
+      {/* Handle outside click */}
+      {isFocus && (
+        <span
+          onClick={() => {
+            setShowDropdown(false);
+            setIsFocus(false);
+          }}
+          ref={outsideSpanRef}
+          className={styles["handle-outside"]}
+        ></span>
+      )}
+    </>
+  );
+}
+Select.propTypes = {
+  options: PropTypes.array,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  setValue: PropTypes.func,
+  name: PropTypes.string,
+};
 
 export default Select;
